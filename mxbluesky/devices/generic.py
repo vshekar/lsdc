30b9824b71b3de9a38fad02d12a308956d4d8a30
vshekar1 <vshekar1@bnl.gov>
2024-11-04 17:44:30 -0500

    Added cs700 PVPositioner
    Added cs700 PVPositioner for both AMX and FMX

--- mxbluesky/devices/generic.py
+++ mxbluesky/devices/generic.py
@@ -1,6 +1,10 @@
 from ophyd import Component as Cpt
 from ophyd import Device, EpicsMotor, EpicsSignal
 from mxbluesky.devices import standardize_readback
+from mxbluesky.devices.base_devices import PVPositionerIsClose
+from ophyd import Device, EpicsMotor, EpicsSignal, EpicsSignalRO
+from ophyd.status import SubscriptionStatus
+from enum import IntEnum, unique
 
 class WorkPositions(Device):
     gx = Cpt(EpicsSignal, "{Gov:Robot-Dev:gx}Pos:Work-Pos")
@@ -33,4 +37,63 @@ class GoniometerStack(Device):
         self.cy = self.py
         self.z = self.pz
         self.cz = self.pz
+<<<<<<< HEAD
         self.omega = self.o
+=======
+        self.omega = self.o
+
+
+class Dewar(Device):
+    rotation = Cpt(EpicsSignal, "{Dew:1-Ax:R}Virtual")
+    rotation_motor = Cpt(EpicsMotor, "{Dew:1-Ax:R}Mtr")
+
+    def rotate(self, rotation_angle, absolute=True):
+        def check_value_sink(*, old_value, value, **kwargs):
+            "Return True when the movement is complete, False otherwise."
+            return old_value == 1 and value == 0
+
+        def check_value_raise(*, old_value, value, **kwargs):
+            "Return True when the movement is started, False otherwise."
+            return old_value == 0 and value == 1
+
+        status = SubscriptionStatus(
+            self.rotation_motor.motor_done_move, check_value_sink
+        )
+        if not self.rotation_motor.motor_done_move.get():
+            raise RuntimeError("Dewar rotation motor already moving.")
+            ### Maybe don't raise an error here but rather do a timeout retry?
+        if absolute:
+            self.rotation.set(rotation_angle)
+        else:
+            current_angle = self.rotation.get()
+            self.rotation.set(current_angle + rotation_angle)
+        status.wait()
+        status = SubscriptionStatus(
+            self.rotation_motor.motor_done_move, check_value_raise
+        )
+        status.wait()
+
+class RobotArm(Device):
+    speed = Cpt(EpicsSignal, '{EMBL}:RobotSpeed')
+
+    def is_full_speed(self):
+        # Checks if the robot speed is 100%
+        if self.speed.get() < 100:
+            return False
+        return True
+
+@unique
+class CryoStreamCmd(IntEnum):
+    START_RAMP = 1
+    STOP_RAMP = 0
+
+
+class CryoStream(PVPositionerIsClose):
+    readback = Cpt(EpicsSignalRO, 'TEMP')
+    setpoint = Cpt(EpicsSignal, 'RTEMP')
+    actuate = Cpt(EpicsSignal, "RAMP.PROC")
+    actuate_value = CryoStreamCmd.START_RAMP
+    stop_signal = Cpt(EpicsSignal, "RAMP.PROC")
+    stop_value = CryoStreamCmd.STOP_RAMP
+
+>>>>>>> 0067019 ([DONOTMERGE] Added cs700 PVPositioner)
