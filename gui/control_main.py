1cd842d4707893779c7fb3718f4b164d38ed461a
vshekar1 <vshekar1@bnl.gov>
2025-12-31 17:56:40 -0500

    Removed manual invocations of callbacks

--- gui/control_main.py
+++ gui/control_main.py
@@ -1662,7 +1662,6 @@ class ControlMain(QtWidgets.QMainWindow):
             if self.protoComboBox.currentText() in (CollectionProtocols.RASTER, 
                                                     CollectionProtocols.STEP_RASTER):
                 self.protoComboBox.setCurrentText(CollectionProtocols.STANDARD)
-                self.protoComboActivatedCB(CollectionProtocols.STANDARD)
             self.showProtParams()
 
     def adjustGraphics4ZoomChange(self, fov):
@@ -2363,7 +2362,6 @@ class ControlMain(QtWidgets.QMainWindow):
             self.choochF2PrimePeak.setText(str(choochResultObj["f2prime_peak"]))
             self.choochResultFlag_pv.put("0")
             self.protoComboBox.setCurrentText(CollectionProtocols.STANDARD)
-            self.protoComboActivatedCB(CollectionProtocols.STANDARD)
         except TypeError as e:
             logger.error(
                 "Chooch plotting failed - check whether scan had a strong signal or not: %s"
@@ -2610,13 +2608,10 @@ class ControlMain(QtWidgets.QMainWindow):
     def protoRadioToggledCB(self, text):
         if self.protoStandardRadio.isChecked():
             self.protoComboBox.setCurrentText(CollectionProtocols.STANDARD)
-            self.protoComboActivatedCB(text)
         elif self.protoRasterRadio.isChecked():
             self.protoComboBox.setCurrentText(CollectionProtocols.RASTER)
-            self.protoComboActivatedCB(text)
         elif self.protoVectorRadio.isChecked():
             self.protoComboBox.setCurrentText(CollectionProtocols.VECTOR)
-            self.protoComboActivatedCB(text)
         else:
             pass
 
@@ -4597,7 +4592,6 @@ class ControlMain(QtWidgets.QMainWindow):
         self.zoom2Radio.setChecked(True)
         self.zoomLevelToggledCB("Zoom2")
         self.protoComboBox.setCurrentText(CollectionProtocols.STANDARD)
-        self.protoComboActivatedCB(CollectionProtocols.STANDARD)
 
     def unmountSampleCB(self):
         logger.info("unmount sample")
